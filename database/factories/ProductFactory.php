0de2666d47a1223bca4feee6962c593d7536ec0c
Endi Iswanto <endy.lasax@gmail.com>
2020-01-18 14:12:04 +0700

    Create relationship product and reviews

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'description' => $faker->paragraph,
+        'price' => $faker->numberBetween($min = 1000, $max = 1000000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween($min = 2, $max = 30)
     ];
 });
